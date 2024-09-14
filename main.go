19c846c10b6e16f64a9859894d9b35cda3a21425
Tom Whittington <Tom@nooksrest.co.uk>
2024-09-14 19:13:28 +0100

    Added example of a struct to handle more complex data types

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 const conferenceTickets uint = 50
 
 var conferenceName = "Go conference"
 var remainingTickets uint = conferenceTickets
-var bookings = make([]map[string]string, 0)
+var bookings = make([]UserData, 0)
+
+type UserData = struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
 
 func main() {
 
@@ -17,11 +23,11 @@ func main() {
 
 	//for { // infinite loop
 	for remainingTickets > 0 && len(bookings) < 50 { //equivalant of a while loop. Set a condition after 'for' keyword
-		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+		userData := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(userData, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
-			bookTicket(firstName, lastName, email, userTickets)
+			bookTicket(userData)
 			firstNames := getFirstNames(bookings)
 			fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
@@ -51,16 +57,16 @@ func greetUsers() {
 	fmt.Println("Get your tickets here to attend")
 }
 
-func getFirstNames(bookings []map[string]string) []string { // return type is put after the parameters
+func getFirstNames(bookings []UserData) []string { // return type is put after the parameters
 	firstNames := []string{}
 
 	for _, booking := range bookings { //typical foreach loop (for index, value:= range list). Use _ to mark as not used
-		firstNames = append(firstNames, booking["firstName"])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
 
-func getUserInput() (string, string, string, uint) {
+func getUserInput() UserData {
 	var firstName string
 	var lastName string
 	var email string
@@ -75,20 +81,19 @@ func getUserInput() (string, string, string, uint) {
 	fmt.Println("Enter number of tickets:	")
 	fmt.Scan(&userTickets)
 
-	return firstName, lastName, email, userTickets
+	return UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTickets,
+	}
 }
 
-func bookTicket(firstName string, lastName string, email string, userTickets uint) {
-	remainingTickets -= userTickets
-
-	var userData = make(map[string]string)
-	userData["firstName"] = firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-	userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+func bookTicket(userData UserData) {
+	remainingTickets -= userData.numberOfTickets
 
 	bookings = append(bookings, userData)
 
-	fmt.Printf("Thankyou %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("Thankyou %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", userData.firstName, userData.lastName, userData.numberOfTickets, userData.email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
